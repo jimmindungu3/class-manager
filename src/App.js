0458104d70ddea213baab9398cef90196921979d
jamesndungu <jimmindungu3@gmail.com>
2023-11-11 12:18:47 +0300

    Add style to navbar, create Analysis component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./components/HomePage";
 import Navbar from "./components/Navbar";
 import RegistrationForm from "./components/RegistrationForm";
 import StudentList from "./components/StudentList";
+import Analysis from "./components/Analysis";
 
 function App() {
   return (
@@ -12,12 +13,10 @@ function App() {
       <div className="container mt-5">
         <div>
           <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route
-              path="RegisterStudent"
-              element={<RegistrationForm />}
-            ></Route>
-            <Route path="UploadMarks" element={<StudentList />}></Route>
+            <Route path="/" element={<HomePage />} />
+            <Route path="RegisterStudent" element={<RegistrationForm />} />
+            <Route path="UploadMarks" element={<StudentList />} />
+            <Route path="Analysis" element={Analysis} />
           </Routes>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import '../App.css'
 
 function Navbar() {
   return (
-  <nav>
-    <Link to="/">Home</Link>  
-    <Link to="RegisterStudent">Register Student</Link> 
-    <Link to="UploadMarks">Upload Marks</Link>
-  </nav>
-  )
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container collapse navbar-collapse justify-content-end">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link link-white">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="RegisterStudent" className="nav-link link-white">Register Student</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="UploadMarks" className="nav-link link-white">Upload Marks</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
